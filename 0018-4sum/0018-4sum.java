c8521efc0aa3ff82dfa0208e6365f55b433aed79
UpmaShukla276 <upma.shukla38@gmail.com>
2026-07-19 14:04:04 +0530

    Time: 22 ms (22.29%), Space: 45.8 MB (46.75%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 3; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+
+                // Skip duplicate j
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        left++;
+                        right--;
+
+                        // Skip duplicate left
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        // Skip duplicate right
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+
+                    } else if (sum < target) {
+
+                        left++;
+
+                    } else {
+
+                        right--;
+
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
