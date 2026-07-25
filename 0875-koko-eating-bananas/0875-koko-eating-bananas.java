05dc88b2669bd1f37ba2d0ce1fd32123942dca26
UpmaShukla276 <upma.shukla38@gmail.com>
2026-07-25 21:45:54 +0530

    Time: 8 ms (68.22%), Space: 47.6 MB (87.78%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int left = 1;
+        int right = 0;
+
+        // Find the maximum pile
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            long hours = 0;
+
+            // Calculate total hours needed at speed = mid
+            for (int pile : piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+
+            if (hours <= h) {
+                // Can finish, try smaller speed
+                right = mid;
+            } else {
+                // Too slow
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
